c4aa5766ba783b40acbf5443131e4da5196f043a
Walid Wazini <wazini25@gmail.com>
2021-12-23 06:56:55 +0800

    first commit

---
+++ lib/Cubit/counter_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+// ignore: unused_import
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counterValue: 0));
+
+  void increment() => emit(CounterState(counterValue: state.counterValue + 1));
+  void reset() => emit(CounterState(counterValue: state.counterValue = 0));
+  void decrement() => emit(CounterState(counterValue: state.counterValue - 1));
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:bloc_concepts/Cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Bloc Concepts'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // ignore: use_key_in_widget_constructors
+  MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // ignore: prefer_final_fields
+  int _counter = 0;
+
+  TextStyle style() {
+    return TextStyle(
+      fontSize: 30,
+      color: Colors.black,
+    );
+  }
+
+  TextStyle style2 = TextStyle(fontSize: 25, color: Colors.black);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state){
+                return Text(
+                  state.counterValue.toString(),
+                  style: style(),
+                );
+              },
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  //tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).reset();
+                  },
+                  //tooltip: 'Increment',
+                  child: Icon(Icons.accessibility),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  //tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
